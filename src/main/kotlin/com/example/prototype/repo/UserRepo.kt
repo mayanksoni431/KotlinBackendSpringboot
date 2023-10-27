1f42d6633c08f49e51ecfdd1c0c7d3b52e0e4c4c
Mayank-dks <Mayank.Soni@dcsg.com>
2023-10-27 14:47:19 +0530

    Revert "added boiler plate"
    This reverts commit d26987a8

--- src/main/kotlin/com/example/prototype/repo/UserRepo.kt
+++
@@ -1,9 +0,0 @@
-package com.example.prototype.repo
-
-import com.example.prototype.repo.model.UserInfo
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-
-
-@Repository
-interface UserRepo : CrudRepository<UserInfo, String>
